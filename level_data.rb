9b0f971367be03429161ac474061ffb0accbbcbe
ここはち <99192596+Kokohachi@users.noreply.github.com>
2023-03-05 11:10:11 +0000

    Change: change source to ChartCyanvas

--- level_data.rb
+++ level_data.rb
@@ -12,10 +12,11 @@ class PSExo
     CPuts.info "譜面の情報を取得します。"
     CPuts.ask "譜面ID（譜面の左上の#から始まるID）を入力して下さい。"
     @chart_id = prompt.ask("譜面ID:") do |q|
-      q.validate(/[A-Za-z0-9]{12}/, "譜面IDのフォーマットに沿っていません。")
+      q = q.sub("/#chcy-/","")
+      q.validate(/[A-Za-z0-9]{23}/, "譜面IDのフォーマットに沿っていません。")
     end
-
-    get = http_get("https://servers.purplepalette.net/sonolus/levels/#{@chart_id}")
+    @chart_id = @chart_id.sub("/#chcy-/","")
+    get = http_get("https://cc.sevenc7c.com/sonolus/levels/#chcy-{@chart_id}")
     if get.code != 200
       CPuts.error "譜面が見つかりませんでした。"
       exit 1
@@ -24,7 +25,7 @@ class PSExo
     chart_data = JSON.parse(get.body.to_s, symbolize_names: true)
     CPuts.info "譜面名：\e[m#{chart_data[:item][:title]}"
     CPuts.info "譜面作者：\e[m#{chart_data[:item][:author]}"
-    data = http_get("https://servers.purplepalette.net" + chart_data[:item][:data][:url])
+    data = http_get("https://cc.sevenc7c.com" + chart_data[:item][:data][:url])
     stream = StringIO.new(data.body.to_s)
 
     Zlib::GzipReader.wrap(stream) do |gz|
